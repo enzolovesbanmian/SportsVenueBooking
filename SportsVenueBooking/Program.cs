5f511db0254a45a20c9937ca275e1099d8b8f47a
enzo <2201887C@student.tp.edu.sg>
2024-12-08 21:42:08 +0800

    added custom identity logic

--- SportsVenueBooking/Program.cs
+++ SportsVenueBooking/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<SportsVenueBookingUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<SportsVenueBookingContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
